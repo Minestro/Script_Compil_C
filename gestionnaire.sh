f969a87708d15e705089cc2ba01c9f292741c2b3
minestro <jacquot.alexandre97@orange.fr>
2015-12-18 10:04:11 +0100

    ajout du menu help

--- gestionnaire.sh
+++ gestionnaire.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+if [ $1 = "-help" ]
+then
+	clear
+	printf "%b\n" 'Usage: ./gestionnaire.sh [fichier]\nFichier est un fichier c++ existant ou pas, donné sans extension.'
+	exit 0
+else
+while true
+do
+	clear
+	echo "voulez vous arrêter ?"
+	read choix
+	[ $reponse in[1-8] ] && break
+done
+fi
